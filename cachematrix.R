e733381affdcc3f8171bcdb02349d996dd52be3d
Qulu-Zheng <806170739@qq.com>
2016-02-06 10:52:46 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+This function is named "makeCacheMtrix"## Put comments here that give an overall description of what your
+This function creates a special "matrix" object and  computes the inverse of the special "matrix".## functions do
 
 ## Write a short comment describing this function
 
